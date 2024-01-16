06da5935fdc4f8ec9988a49b828764ff835a4008
AhnHongchan <hha0408@gmail.com>
2024-01-16 11:05:33 +0900

    second commit

---
+++ dict_practice.py
@@ -0,0 +1,14 @@
+# my_dict = {
+#     'apple' : 12,
+#     'list' : [1, 2, 3],
+#     'apple' : 100
+# }
+# print(my_dict)
+
+print(1 == True) # True
+# 암시적 형변환 'True를 1로'
+print(1 is True) # "is" with a literal. Did you mean "=="?
+# 메모리 주소가 전혀 다름
+
+vowels = 'aeiou'
+print(('b' and 'a') in vowels)
